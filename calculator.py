57d99d444004645f75a7de1310072db0110cc797
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-17 11:22:57 +0900

    출력장과 폭 설정

--- calculator.py
+++ calculator.py
@@ -4,5 +4,8 @@ import tkinter as tk
 calculator = tk.Tk()
 calculator.title("Jinkyu Calulator")
 calculator.geometry("500x500")
-
+# display이라는 이름의 출력장 추가, 폭은 25
+display = tk.Entry(calculator, width=25)
+# 위치를 정해는주는 명령어
+display.pack()
 calculator.mainloop()
